423b2ec59d730c263b048285ce5811f57e0cfe53
Kezia Ardia Sasaki <keziaardia03@gmail.com>
2025-10-21 21:19:18 +0800

    Add files via upload

---
+++ Meet1.py
@@ -0,0 +1,35 @@
+# class -> Motor
+# object -> surat2, kondisi_mesin, merk, model, harga, cc, warna
+# nama_Nim
+# Nama_OOP
+
+class kendaraan :
+    def __init__(self, merk, model, warna, cc, kondisi_mesin, surat2, harga):
+        self.merk = merk
+        self.model = model
+        self.warna = warna
+        self.cc = cc
+        self.kondisi_mesin = kondisi_mesin
+        self.surat2 = surat2
+        self.harga = harga
+
+    def tampilkan_info(self):
+        print(f"merk           : {self.merk}")
+        print(f"model          : {self.model}")
+        print(f"warna          : {self.warna}")
+        print(f"cc             : {self.cc} cc")
+        print(f"kondisi mesin  : {self.kondisi_mesin}")
+        print(f"surat-surat    : {self.surat2}")
+        print(f"harga          : Rp{self.harga:,}")
+
+mobil = kendaraan(
+    merk="mitsubishi",
+    model="pajero sport",
+    warna="putih",
+    cc=2400,
+    kondisi_mesin="Baik",
+    surat2="Lengkap (STNK & BPKB)",
+    harga=600000000
+)
+
+mobil.tampilkan_info()
